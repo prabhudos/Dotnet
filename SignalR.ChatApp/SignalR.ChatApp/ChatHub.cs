640113cfd499ba0a408789680246e070a1b3ee4f
prabhu <leoprabhu89@gmail.com>
2018-05-30 12:09:20 +0530

    Initial Checkin
    Initial Checkin

---
+++ SignalR.ChatApp/SignalR.ChatApp/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR.ChatApp
+{
+    public class ChatHub : Hub
+    {
+        public void Join(string room)
+        {
+            Groups.Add(Context.ConnectionId, room);
+        }
+
+        public void Leave(string room)
+        {
+            Groups.Remove(Context.ConnectionId, room);
+        }
+
+        public void Send(string name, string room, string message)
+        {
+            Clients.Group(room).broadcastMessage(name, message);
+            //Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
---
+++ SignalR.ChatApp/SignalR.ChatApp/Scripts/chat.js
@@ -0,0 +1,30 @@
+﻿(function () { // Angular encourages module pattern, good!
+    var app = angular.module('myApp', []);
+        
+    var hub = $.connection.chatHub; // create a proxy to signalr hub on web server
+
+    app.controller('myCtrl', ['$http', '$scope', function ($http, $scope) {
+
+        //Gets message from HUB
+        hub.client.broadcastMessage = function (name, message) {
+            $('#discussion').append('<li><strong>' + name + '</strong>: ' + message + '</li>');
+        }
+
+        //Join a chat
+        $scope.join = function (message) {
+            hub.server.join($scope.room);
+        }
+
+        //Leave a chat
+        $scope.leave = function (message) {
+            hub.server.leave($scope.room);
+        }
+
+        //Send message to chat
+        $scope.send = function (message) {                        
+            hub.server.send($scope.name, $scope.room, message);
+        }
+
+        $.connection.hub.start(); // connect to signalr hub
+    }]);
+})();
